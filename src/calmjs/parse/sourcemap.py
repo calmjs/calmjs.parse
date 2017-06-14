ddf1d4e05fdf25be6378f3934f10f5801a714db7
Tommy Yu <tommy.yu@auckland.ac.nz>
2017-06-14 23:27:58 +1200

    Class for bookkeeping of states
    - Just provide this as an asymmetric attribute store.

--- src/calmjs/parse/sourcemap.py
+++ src/calmjs/parse/sourcemap.py
@@ -31,3 +31,63 @@ class Names(object):
     def __iter__(self):
         for name, idx in sorted(self._names.items(), key=lambda x: x[1]):
             yield name
+
+
+class Bookkeeper(object):
+    """
+    A class for tracking positions
+
+    Set a current position, read out a delta compared to the previous
+    for a given attribute
+    """
+
+    def __init__(self):
+        super(Bookkeeper, self).__setattr__('_prev', {})
+        super(Bookkeeper, self).__setattr__('_curr', {})
+
+    def _hasattr(self, attr):
+        return all(
+            isinstance(check.get(attr, None), int)
+            for check in (self._prev, self._curr)
+        )
+
+    def __setattr__(self, attr, value):
+        """
+        Set the current position
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("assignment must be of type 'int'")
+
+        if not self._hasattr(attr):
+            self._curr[attr] = self._prev[attr] = value
+        else:
+            self._curr[attr], self._prev[attr] = value, self._curr[attr]
+
+    def __getattr__(self, attr):
+        if not self._hasattr(attr):
+            raise AttributeError("'%s' object has no attribute %r" % (
+                self.__class__.__name__, attr))
+        return self._curr[attr] - self._prev[attr]
+
+    def __delattr__(self, attr):
+        if not self._hasattr(attr):
+            raise AttributeError("'%s' object has no attribute %r" % (
+                self.__class__.__name__, attr))
+        self._prev[attr] = self._curr[attr] = 0
+
+
+def default_book():
+    book = Bookkeeper()
+    # index of the current file can be implemented/tracked with the
+    # Names class.
+
+    # position of the current line that is being written; 0-indexed as
+    # there are no existing requirements, and that it maps directly to
+    # the length of the string written (usually).
+    book.sink_column = 0
+    # since the source line/col positions have been implemented as
+    # 1-indexed values, so the offset is pre-applied like so.
+    book.source_line = 1
+    book.source_column = 1
+    return book
--- src/calmjs/parse/tests/test_sourcemap.py
+++ src/calmjs/parse/tests/test_sourcemap.py
@@ -28,3 +28,64 @@ class NameTestCase(unittest.TestCase):
 
         self.assertEqual(
             list(names), ['hello', 'world', 'goodbye', 'person', 'people'])
+
+
+class BookkeeperTestCase(unittest.TestCase):
+
+    def test_empty(self):
+        book = sourcemap.Bookkeeper()
+        with self.assertRaises(AttributeError) as e:
+            book.foo
+        self.assertEqual(
+            e.exception.args[0], "'Bookkeeper' object has no attribute 'foo'")
+
+        with self.assertRaises(AttributeError) as e:
+            del book.foo
+
+    def test_typecheck(self):
+        book = sourcemap.Bookkeeper()
+        with self.assertRaises(TypeError) as e:
+            book.foo = 'notanint'
+        self.assertEqual(
+            e.exception.args[0], "assignment must be of type 'int'")
+
+    def test_usage(self):
+        book = sourcemap.Bookkeeper()
+        book.foo = 123
+        self.assertEqual(book.foo, 0)
+        self.assertEqual(book.foo, 0)
+        book.foo = 124
+        self.assertEqual(book.foo, 1)
+        self.assertEqual(book.foo, 1)
+        book.foo = 1
+        self.assertEqual(book.foo, -123)
+        self.assertEqual(book.foo, -123)
+        book.foo = 1
+        self.assertEqual(book.foo, 0)
+        self.assertEqual(book.foo, 0)
+        book.foo = 1
+        self.assertEqual(book.foo, 0)
+        self.assertEqual(book.foo, 0)
+        book.foo = 2
+        book.foo = 2
+        self.assertEqual(book.foo, 0)
+
+    def test_reset(self):
+        book = sourcemap.Bookkeeper()
+        book.foo = 123
+        book.foo = 124
+        self.assertEqual(book.foo, 1)
+        del book.foo
+        self.assertEqual(book.foo, 0)
+
+    def test_default_book(self):
+        book = sourcemap.default_book()
+        self.assertEqual(book.sink_column, 0)
+        self.assertEqual(book.source_line, 0)
+        self.assertEqual(book.source_column, 0)
+        book.sink_sink_column = 0
+        book.source_line = 1
+        book.source_column = 1
+        self.assertEqual(book.sink_column, 0)
+        self.assertEqual(book.source_line, 0)
+        self.assertEqual(book.source_column, 0)
