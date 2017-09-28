89327a3461c14a46742ba2ec6167b96f6419e8dd
Tommy Yu <tommy.yu@auckland.ac.nz>
2017-09-28 15:27:46 +1300

    Begin next dev version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ class InstallHook(install):
             call, [sys.executable, '-m', 'calmjs.parse.parsers.optimize'])
 
 
-version = '1.0.0'
+version = '1.1.0'
 
 classifiers = """
 Development Status :: 5 - Production/Stable
