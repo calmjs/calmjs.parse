8187024a1109fa9fabdc42886154714136bfe526
Tommy Yu <y@metatoaster.com>
2019-11-21 15:34:04 +1300

    preparing calmjs.parse-1.2.2.pre

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ class InstallHook(install):
             call, [sys.executable, '-m', 'calmjs.parse.parsers.optimize'])
 
 
-version = '1.2.1'
+version = '1.2.2'
 
 classifiers = """
 Development Status :: 5 - Production/Stable
